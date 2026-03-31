a2521792f460fa38f74ed968c465ac19124ffaa7
poosana1 <poosana.dev@gmail.com>
2026-03-31 16:04:17 +0700

    Basic ETL concept

---
+++ main.py
@@ -0,0 +1,53 @@
+import requests
+import psycopg2
+from dotenv import load_dotenv
+import os
+
+# Create Database Connection
+load_dotenv()
+
+conn = psycopg2.connect(
+    host="localhost",
+    port=os.getenv("POSTGRES_PORT"),
+    database=os.getenv("POSTGRES_DB"),
+    user=os.getenv("POSTGRES_USER"),
+    password=os.getenv("POSTGRES_PASSWORD")
+)
+
+# Extract
+response = requests.get("https://api.open-meteo.com/v1/forecast?latitude=18.7883&longitude=98.9853&hourly=temperature_2m")
+data = response.json()
+hourly_time = data["hourly"]["time"]
+hourly_temperature = data["hourly"]["temperature_2m"]
+
+# Transform
+listData = []
+for time, temperature in zip(hourly_time, hourly_temperature):
+    listData.append({
+        "time": time,
+        "temperature": temperature
+    })
+
+print(listData)
+
+
+# Create Table
+cursor = conn.cursor()
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS weather_data (
+    id SERIAL PRIMARY KEY,
+    time TIMESTAMP,
+    temperature FLOAT
+)
+""")
+conn.commit()
+
+# Load
+for item in listData:
+    cursor.execute("""
+    INSERT INTO weather_data (time, temperature) VALUES (%s, %s)
+    """, (item["time"], item["temperature"]))
+conn.commit()
+
+cursor.close()
+conn.close()
